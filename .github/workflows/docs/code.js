e2779048371c89f825d8bd72acd2fe1c2559924d
Bigmac9995 <lucajens1@gmail.com>
2023-09-11 14:58:12 +0200

    buttons yes

--- .github/workflows/docs/code.js
+++ .github/workflows/docs/code.js
@@ -5,40 +5,124 @@ var choices = "";
 
 function background_change(){
     
-    if(choices=="1"){
+    if(choices==""){
         
-            document.getElementById("BackBody").style.backgroundImage="url(img/Ending_TB.jpg)";
+            document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
             
             document.getElementById("text").innerHTML = "";
         
             document.getElementById("button_text").innerHTML = "Take the valley.";
             document.getElementById("button_text2").innerHTML = "Take the River.";
-        
+            choices = choices + "0";
     }
+    /* 1 */
+    else if(choices=="0"){
+        
+        document.getElementById("BackBody").style.backgroundImage="url(img/Bank_robbing.jpg)";
+        
+        document.getElementById("text").innerHTML = "";
+    
+        document.getElementById("button_text").innerHTML = "Ligma.";
+        document.getElementById("button_text2").innerHTML = "Take the River.";
+        choices = choices + "0";
+}
+else if(choices=="1"){
+        
+    document.getElementById("BackBody").style.backgroundImage="url(img/Train_robbing.jpg)";
+    
+    document.getElementById("text").innerHTML = "";
+
+    document.getElementById("button_text").innerHTML = "Sugma.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+    choices = choices + "0";
+}
+/* 2 */
+else if(choices=="00"){
+        
+    document.getElementById("BackBody").style.backgroundImage="url(img/Bank_robbing.jpg)";
+    
+    document.getElementById("text").innerHTML = "";
+
+    document.getElementById("button_text").innerHTML = "Ginger.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+    choices = choices + "0";
+}
+else if(choices=="01"){
+        
+    document.getElementById("BackBody").style.backgroundImage="url(img/Bank_robbing.jpg)";
+    
+    document.getElementById("text").innerHTML = "";
+
+    document.getElementById("button_text").innerHTML = "Ligma.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+    choices = choices + "0";
+}
+else if(choices=="10"){
+    
+    document.getElementById("BackBody").style.backgroundImage="url(img/Train_robbing.jpg)";
+    
+    document.getElementById("text").innerHTML = "";
+    
+    document.getElementById("button_text").innerHTML = "Fuck You.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+    choices = choices + "0";
+    }
+else if(choices=="11"){
+    
+document.getElementById("BackBody").style.backgroundImage="url(img/Train_robbing.jpg)";
+
+document.getElementById("text").innerHTML = "";
+
+document.getElementById("button_text").innerHTML = "Sugma.";
+document.getElementById("button_text2").innerHTML = "Take the River.";
+choices = choices + "0";
+}
+
     else{
-        document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
+    /*     document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
     
     document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
 
     document.getElementById("button_text").innerHTML = "Take the valley.";
     document.getElementById("button_text2").innerHTML = "Take the River.";
 
-    choices="1";
+    choices="1"; */
     }
 }
 
 
 
 function background_change2(){
-    document.getElementById("BackBody").style.backgroundImage="url(img/Ending_Neutral.jpg)";
+    if(choices==""){
+        
+        document.getElementById("BackBody").style.backgroundImage="url(img/Train_outside.webp)";
+        
+        document.getElementById("text").innerHTML = "";
+    
+        document.getElementById("button_text").innerHTML = "Take the valley.";
+        document.getElementById("button_text2").innerHTML = "Take the River.";
+    choices = choices + "1";
+}
+/* else if(choices=="1"){
+        
+    document.getElementById("BackBody").style.backgroundImage="url(img/Train_robbing.jpg)";
+    
+    document.getElementById("text").innerHTML = "";
 
-    document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
-    
-    document.getElementById("button_text").innerHTML = "Take the valley.";
+    document.getElementById("button_text").innerHTML = "Sugma.";
     document.getElementById("button_text2").innerHTML = "Take the River.";
-    
-    choices="0";
-    
+    choices = choices + "1";
+} */
+else{
+    /* document.getElementById("BackBody").style.backgroundImage="url(img/Ending_Neutral.jpg)";
+
+document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
+
+document.getElementById("button_text").innerHTML = "Take the valley.";
+document.getElementById("button_text2").innerHTML = "Take the River.";
+
+choices="0";*/
+} 
 }
 
 
