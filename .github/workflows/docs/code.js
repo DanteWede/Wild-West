990e1b783ffafd07847d538f15201e3717ef6fee
Bigmac9995 <lucajens1@gmail.com>
2023-09-11 13:40:59 +0200

    shit

--- .github/workflows/docs/code.js
+++ .github/workflows/docs/code.js
@@ -1,10 +1,46 @@
+var choices = "";
+
+
+
+
 function background_change(){
-    document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
+    
+    if(choices=="1"){
+        
+            document.getElementById("BackBody").style.backgroundImage="url(img/Ending_TB.jpg)";
+            
+            document.getElementById("text").innerHTML = "";
+        
+            document.getElementById("button_text").innerHTML = "Take the valley.";
+            document.getElementById("button_text2").innerHTML = "Take the River.";
+        
+    }
+    else{
+        document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
     
     document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
 
     document.getElementById("button_text").innerHTML = "Take the valley.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+
+    choices="1";
+    }
 }
+
+
+
+function background_change2(){
+    document.getElementById("BackBody").style.backgroundImage="url(img/Ending_Neutral.jpg)";
+
+    document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
+    
+    document.getElementById("button_text").innerHTML = "Take the valley.";
+    document.getElementById("button_text2").innerHTML = "Take the River.";
+    
+    choices="0";
+    
+}
+
 
 
 
