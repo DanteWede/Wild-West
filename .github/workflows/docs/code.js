83add4ddef6c72665331ff067129827762928680
Bigmac9995 <lucajens1@gmail.com>
2023-09-11 09:48:51 +0200

    Getting the text and button text to change on click

--- .github/workflows/docs/code.js
+++ .github/workflows/docs/code.js
@@ -1,5 +1,9 @@
 function background_change(){
     document.getElementById("BackBody").style.backgroundImage="url(img/Bank_outside.webp)";
+    
+    document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph. waihubdauibdua daiuhwndioanidnh awdiuabwidbnaoi doaiuhwdiaobhwdu aowdboauibwduioanwdiubaidba idbaoibwduiabudbauiob dauio";
+
+    document.getElementById("button_text").innerHTML = "Take the valley.";
 }
 
 
