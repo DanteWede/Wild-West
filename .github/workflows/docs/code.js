c698da2eacee25afbd93e7d955a1f9f85d5ed975
MarFri8 <marfri8@icloud.com>
2023-09-27 11:39:47 +0200

    Update code.js

--- .github/workflows/docs/code.js
+++ .github/workflows/docs/code.js
@@ -724,13 +724,13 @@ exitIds: [0,0]
 {/* TB, u suck! */
 id: 46,
 background: "url(img/Death2.jpg)",
-desc: `<strong>I</strong>n a high-stakes gamble for wealth and loyalty, you made the daring decision to stay with Dutch and Micha in their desperate attempt to secure the gold, despite the imminent danger and the approaching law enforcement.
+desc: `<strong>Y</strong>ou suddenly collapsed to the ground, wracked by a violent fit of coughing.
 <br><br>
-As the footsteps drew nearer, the tension in the vault was palpable. Dutch and Micha were determined to make the most of this opportunity, even as the police closed in. The prospect of riches was too alluring to pass up, and you knew the risks involved.
+The gang gathered around you, their expressions filled with concern and fear. It was a scene of utter helplessness as you struggled to breathe, your body wracked by the debilitating effects of tuberculosis.
 <br><br>
-However, as the police entered the vault and discovered your presence, the situation quickly turned fatal. Without hesitation, the police officers opened fire, and in an instant, your life was cut short. The heist had come to a tragic and violent end, leaving the stolen gold behind and sealing your fate in this lawless and unforgiving world.
+Your fellow gang members, who had faced danger and adversity together, now watched in shock as you succumbed to this deadly disease. Despite their efforts to provide comfort and aid, there was nothing they could do to save you from the relentless grip of tuberculosis.
 <br><br>
-The decisions made during this heist had led to a devastating outcome, forever altering the course of the Van Der Linde gang and leaving an indelible mark on the history of this perilous land.                         
+In this unforgiving and lawless land, even the strongest and most determined could fall victim to the merciless forces of nature and illness. Your sudden and untimely death served as a stark reminder of the harsh realities faced by the Van Der Linde gang, a group bound together by loyalty and the pursuit of fortune, but ultimately vulnerable to the whims of fate..                         
 `,
 exitTexts: ["Play Again", "End"],
 exitIds: [0,0]
